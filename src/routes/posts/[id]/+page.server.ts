f35d56b65300c9f9a8d21c5fa372870a568a30fe
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2022-09-02 12:26:06 +1000

    Add non constrained version
    Signed-off-by: Ivan Vanderbyl <ivanvanderbyl@gmail.com>

---
+++ src/routes/posts/[id]/+page.server.ts
@@ -0,0 +1,8 @@
+import type { PageServerLoad } from './$types';
+export const prerender = true;
+export const load: PageServerLoad = async ({ params }) => {
+	return {
+		id: params.id,
+		server: true
+	};
+};
