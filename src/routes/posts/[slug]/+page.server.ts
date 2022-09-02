1210a444c59e54a7f1bf4d5952027eb71505ecff
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2022-09-02 12:27:55 +1000

    Try slug
    Signed-off-by: Ivan Vanderbyl <ivanvanderbyl@gmail.com>

--- src/routes/posts/[slug]/+page.server.ts
+++ src/routes/posts/[slug]/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 export const prerender = true;
 export const load: PageServerLoad = async ({ params }) => {
 	return {
-		id: params.id,
+		id: params.slug,
 		server: true
 	};
 };
