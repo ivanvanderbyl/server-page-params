65c42e6230bdbe4e8c5d22457caa12f6f492afd3
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2022-09-02 12:24:24 +1000

    Prerender all
    Signed-off-by: Ivan Vanderbyl <ivanvanderbyl@gmail.com>

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,18 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true,
+			sourceMap: true,
+			preserve: ['ld+json'],
+			typescript: true
+		})
+	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		inlineStyleThreshold: 15000
 	}
 };
 
